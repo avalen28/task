d5d5908d796aaf79ed768a91a46ab48948c07be4
Alberto Valenzuela <avalen28@gmail.com>
2023-05-05 21:41:27 +0200

    Adds endpoint to create a task with the corresponding sql query

--- db/queries.js
+++ db/queries.js
@@ -8,3 +8,24 @@ const pool = new Pool({
   password: process.env.PASSWORD_DB,
   port: process.env.PORT_DB,
 });
+
+// Las tareas deben tener un título, una descripción, una fecha de vencimiento y un estado
+const createTask = (req, res) => {
+  const {
+    title, description, deadline, isCompleted,
+  } = req.body;
+
+  const formattedDeadline = new Date(deadline).toGMTString();
+  pool.query(
+    'INSERT INTO tasks (title, description, deadline, is_completed) VALUES ($1, $2, $3, $4)',
+    [title, description, formattedDeadline, isCompleted],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).send(`${results.rowCount} tasks added to DB`);
+    },
+  );
+};
+
+module.exports = { createTask };
--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
+const db = require('../db/queries');
 
 // @desc    Test endpoint for the API
-// @route   GET /
+// @route   GET /tasks/test
 // @access  Public
 router.get('/test', async (req, res) => {
   res.json({ message: 'This is a test endpoint for the task manager project' });
 });
+
+// @desc    Create new Task
+// @route   POST /tasks/create
+// @access  Public
+router.post('/create', db.createTask);
 
 module.exports = router;
