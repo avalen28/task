63cfeb30ad9d6f770ecd33a2c0d9a42d6ebf9a21
Alberto Valenzuela <avalen28@gmail.com>
2023-05-05 14:32:54 +0200

    adds eslint with AirBnB configuration to project and body Prase to project dependencies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser'); // ESlint gives an error but package is installed.
 
 const app = express();
 const port = process.env.PORT;
@@ -8,5 +8,5 @@ app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  })
+  }),
 );
