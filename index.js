b6c24300a4f04c6338d5bbf8a36acb82f7c86f4a
Alberto Valenzuela <avalen28@gmail.com>
2023-05-05 13:52:57 +0200

    Adds dotenv as project dependency. Adds .env file with initial port variable. Adds index.js with first structure

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = process.env.PORT;
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
