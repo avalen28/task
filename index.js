18c6ea7e2f3acc8211795b23a307c4ba28ed36e3
Alberto Valenzuela <avalen28@gmail.com>
2023-05-05 15:51:05 +0200

    Adds routes folder with task.js . Imports this .js to index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser'); // ESlint gives an error but package is installed.
 
+// Routes
+const taskRouter = require('./routes/task');
+
 const app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json());
@@ -10,3 +13,6 @@ app.use(
     extended: true,
   }),
 );
+
+// Routes intro
+app.use('/tasks', taskRouter);
