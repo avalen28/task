6c7789465330282c51b28cec09f123c374f6ce33
Alberto Valenzuela <avalen28@gmail.com>
2023-05-07 09:46:26 +0200

    Adds unit test for a endpoint /tasks/pending checking for response status, response type and presence of pending tasks only

--- test/test.js
+++ test/test.js
@@ -30,3 +30,17 @@ describe('GET /tasks/all', () => {
     expect(response.data).to.be.an('array');
   });
 });
+
+describe('GET /tasks/pending', () => {
+  it('returns a json with an array of pending tasks', async () => {
+    // Act
+    const response = await axios.get(`${apiUrl}/pending`);
+    // Assert
+    expectResponseStatus(response.status, 200);
+    expect(response.data).to.be.an('array');
+    if (response.data.length > 0) {
+      const pendingTasks = response.data.filter((task) => task.is_completed === false);
+      expect(pendingTasks.length).equal(response.data.length);
+    }
+  });
+});
